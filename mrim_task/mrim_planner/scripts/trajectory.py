c2166ed76fc395d01505e9c6dc772920bc8b6802
Petr Gabrlik <petr.gabrlik@ceitec.vutbr.cz>
2022-08-04 15:16:08 +0200

    Collision prevention completed.

--- mrim_task/mrim_planner/scripts/trajectory.py
+++ mrim_task/mrim_planner/scripts/trajectory.py
@@ -161,9 +161,9 @@ class TrajectoryUtils():
 
         wps_interp = []
 
-        print("********************** waypoints **********************")
-        for i in range(len(waypoints)):
-            print("waypoint {}: {} {}".format(i, waypoints[i].point, waypoints[i].heading))
+        # print("********************** waypoints **********************")
+        # for i in range(len(waypoints)):
+        #     print("waypoint {}: {} {}".format(i, waypoints[i].point, waypoints[i].heading))
 
         idx = 0
         while idx != (len(waypoints) - 1):
@@ -195,7 +195,7 @@ class TrajectoryUtils():
 
             # compute the heading change per the unit of distance (meter) for the current segment
             hdg_per_dist = d_hdg / subtraj_len
-            print("hdg_per_dist: {}".format(hdg_per_dist))
+            # print("hdg_per_dist: {}".format(hdg_per_dist))
 
             # interpolate headings
             for i in range(1, len(subtraj) - 1):
@@ -226,8 +226,8 @@ class TrajectoryUtils():
         wps_interp.append(waypoints[-1])
 
         # print the interpolated waypoints
-        for i in range(len(wps_interp)):
-            print("waypoint {}: {} {}".format(i, wps_interp[i].point, wps_interp[i].heading))
+        # for i in range(len(wps_interp)):
+        #     print("waypoint {}: {} {}".format(i, wps_interp[i].point, wps_interp[i].heading))
 
         return wps_interp
 
@@ -625,42 +625,67 @@ class TrajectoryUtils():
         ## |  [COLLISION AVOIDANCE METHOD #2]: Delay UAV with shorter trajectory at start until there is no collision occurring  |
         elif method == 'delay_till_no_collisions_occur':
 
-            raise NotImplementedError('[STUDENTS TODO] Collision prevention method \'delay_till_no_collisions_occur\' not finished. You have to finish it on your own.')
+            # raise NotImplementedError('[STUDENTS TODO] Collision prevention method \'delay_till_no_collisions_occur\' not finished. You have to finish it on your own.')
             # Tips:
             #  - you might select which trajectory it is better to delay
             #  - the smallest delay step is the sampling step stored in variable 'self.dT'
 
             delay_step = self.dT
+            print("delay step: ", delay_step)
             traj_times = [t.getTime() for t in trajectories]
-            traj_lens  = [t.getLength() for t in trajectories]
+            traj_lens = [t.getLength() for t in trajectories]
+            print("/******************* traj times *******************")
+            print("traj times: ", traj_times)
+            print("/******************* traj lengths *******************")
+            print("traj length: ", traj_lens)
 
             # Decide which UAV should be delayed
             # [STUDENTS TODO] CHANGE BELOW
-            delay_robot_idx, nondelay_robot_idx = 0, 1
+            # delay_robot_idx, nondelay_robot_idx = 0, 1
+            if traj_times[0] >= traj_times[1]:
+                delay_robot_idx = 1
+                nondelay_robot_idx = 0
+                print("delay second UAV")
+            else:
+                delay_robot_idx = 0
+                nondelay_robot_idx = 1
+                print("delay first UAV")
 
             # TIP: use function `self.trajectoriesCollide()` to check if two trajectories are in collision
             collision_flag, collision_idx = \
-                self.trajectoriesCollide(trajectories[delay_robot_idx], trajectories[nondelay_robot_idx], safety_distance)
+                self.trajectoriesCollide(trajectories[delay_robot_idx], trajectories[nondelay_robot_idx],
+                                         safety_distance)
+
+            print("collision_idx: ", collision_idx)
 
             i = 0
             while collision_flag:
 
                 # delay the shorter-trajectory UAV at the start point by sampling period
 
-                if i < 10:
-                    delay_t += delay_step
-                    # TIP: use function `trajectory.delayStart(X)` to delay a UAV at the start location by X seconds
-                    trajectories[delay_robot_idx].delayStart(delay_step)
-                else:
-                    collision_flag = False
+                # if i < 10:
+                #     delay_t += delay_step
+                #     # TIP: use function `trajectory.delayStart(X)` to delay a UAV at the start location by X seconds
+                #     trajectories[delay_robot_idx].delayStart(delay_step)
+                #     i += 1
+                # else:
+                #     collision_flag = False
+                #     break
+                delay_t += delay_step
+                trajectories[delay_robot_idx].delayStart(delay_step)
 
-        # # #}
+                collision_flag, collision_idx = \
+                    self.trajectoriesCollide(trajectories[delay_robot_idx], trajectories[nondelay_robot_idx],
+                                             safety_distance)
+
+
+            # # #}
 
         if delay_robot_idx is not None:
             delayed_robots, delays = [delay_robot_idx], [delay_t]
 
         return trajectories, delayed_robots, delays
-    # # #}
+        # # #}
 
     ## | ---------------- Functions: path smoothing --------------- |
 
